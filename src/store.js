a58b2deba3029de7431ee5dc9ebe001a58ad8fe4
voidnoble <cezips@gmail.com>
2020-10-16 17:04:05 +0900

    4.1 CreateAction

--- src/store.js
+++ src/store.js
@@ -1,28 +1,15 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text
-  };
-};
-
-const deleteTodo = (id) => {
-  return {
-    type: DELETE,
-    id
-  };
-};
+const addToDo = createAction('ADD');
+const deleteTodo = createAction('DELETE');
 
 const reducer = (state = [], action) => {
   switch(action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteTodo.type:
+      return state.filter(toDo => toDo.id !== action.payload);
     default:
       return state;
   }
